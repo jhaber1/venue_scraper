99b0247fe75b1ab6108197a0ff31c61130d61342
Jake Haber <singjsong@gmail.com>
2017-04-09 11:57:44 -0500

    More progress
    Paginate through all of a user's artists to get all of them
    Intersection checker
    Refactoring

--- lib/venue_scraper.ex
+++ lib/venue_scraper.ex
@@ -15,26 +15,42 @@ defmodule VenueScraper do
   @lastfm_url "https://ws.audioscrobbler.com/2.0/"
   @lastfm_api_key Application.get_env(:venue_scraper, :lastfm_api_key)
 
-  def get_bands(user \\ "SP420") do
-    url = @lastfm_url <> "?method=user.gettopartists&user=#{user}&api_key=#{@lastfm_api_key}&format=json&limit=10"
-
-    HTTPoison.get!(url).body
-    |> Poison.decode!
-    |> Map.get("topartists")
-    |> Map.get("artist")
-    |> Enum.map(&(&1["name"]))
+  def get_bands_for(user \\ "SP420") do
+    url = @lastfm_url <> "?method=user.gettopartists&user=#{user}&api_key=#{@lastfm_api_key}&format=json&period=overall&limit=500"
+    fetch_bands(url, 1)
   end
 
-  def swb do
+  def fetch_bands(url, page, artists \\ [])
+  def fetch_bands(url, page, artists) do
+    case HTTPoison.get(url <> "&page=#{page}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        new_artists = Poison.decode!(body)
+        |> Map.get("topartists")
+        |> Map.get("artist")
+        |> Enum.map(&(&1["name"]))
+
+        if Enum.count(new_artists) > 0 do
+          fetch_bands(url, page + 1, artists ++ new_artists)
+        else
+          artists ++ new_artists
+        end
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect(reason)
+    end
+  end
+
+  def sw do
     # url = "https://www.thesidewinderaustin.com/"
     # HTTPoison.get!(url).body
     keys = ~W[title presents description id starts_at doors_at]
 
-    events = File.read!("./lib/20170409_sidewinder.json") |> Poison.decode!
-    for event <- events do
-      Enum.reduce(event, %{}, fn({k, v}, result) ->
-        if Enum.member?(keys, k), do: Map.put(result, k, v), else: result
-      end)
-    end
+    File.read!("./lib/20170409_sidewinder.json")
+    |> Poison.decode!
+    |> Enum.map(&(Map.take(&1, keys)))
+  end
+
+  def bands_at_venue?(user_bands, venue_bands) do
+    intersection = MapSet.intersection(MapSet.new(user_bands), MapSet.new(venue_bands))
+    MapSet.size(intersection) > 0
   end
 end
