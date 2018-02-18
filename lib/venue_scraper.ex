1cc83e9c6985783bbca44fa2b046809598102bce
Jake Haber <singjsong@gmail.com>
2018-02-18 09:26:18 -0600

    And more progress
    Improve timestamp parsing for CATIL
    Better indexing
    Basic ES search for indexed events

--- lib/venue_scraper.ex
+++ lib/venue_scraper.ex
@@ -1,4 +1,4 @@
-import Tirexs.HTTP
+import Tirexs.{HTTP, Search, Query}
 # import UUID
 
 defmodule VenueScraper do
@@ -37,8 +37,8 @@ defmodule VenueScraper do
   @lastfm_api_key Application.get_env(:venue_scraper, :lastfm_api_key)
 
   # Elasticsearch index
-  @music "/music"
-  @events @music <> "/events"
+  @music_path "/music"
+  @events_path @music_path <> "/events"
 
   # Fetch bands for a given last.fm user
   def get_bands_for(user \\ "SP420") do
@@ -77,9 +77,9 @@ defmodule VenueScraper do
 
    # Come and Take It Live
   def catil do
-    # url = "http://www.ticketfly.com/venue/22421-come-and-take-it-live/"
-    # HTTPoison.get!(url).body
 
+    # url = "https://www.ticketfly.com/venue/22421-come-and-take-it-live/"
+    # HTTPoison.get!(url).body
     File.read!("./lib/20170514_catil.html")
     |> Floki.find("div.event-results ul")
 
@@ -88,31 +88,40 @@ defmodule VenueScraper do
 
     # returns list of li elements (nodes are {tag_name, attributes, children_nodes})
     |> elem(2)
-
     |> Enum.map(fn(li) ->
-      # TODO: Determined difference between starts_at/doors_at, need to look at next node over
+      # TODO: Determine difference between starts_at/doors_at, need to look at next node over
       presents = Floki.find(li, "div:nth-child(2) p.event-results-sponsor") |> Floki.text
       title = Floki.find(li, "div:nth-child(2) h3") |> Floki.text
 
+      # TODO: Move to "presents" and make it an array since this is almost always comma-separated bands
       # TODO: grab event show link from here
       description = Floki.find(li, "div:nth-child(2) p.description") |> Floki.text
 
-      # TODO: may/may not have timezone attached (if not, probably safe to assume time is correct, and zone is not)
-      starts_at = Floki.find(li, "div.event-date span") |> Floki.attribute("title") |> Enum.at(0)
+      # The timezone offset can be like 3 different ones so it's crap, but the actual hours/minute/seconds seem to be
+      # consistently in the correct timezone of the parser, so we strip out the timezone offset
+      starts_at_raw = Floki.find(li, "div.event-date span")
+        |> Floki.attribute("title")
+        |> Enum.at(0)
+        |> (&Regex.run(~R/\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}/, &1)).()
+        |> Enum.at(0)
+
+      # Parse similarly to what is done for queueapp JSON
+      starts_at = Timex.parse("#{starts_at_raw} #{Timex.Timezone.Local.lookup}", "{YYYY}-{0M}-{0D}T{h24}:{m}:{s} {Zname}")
+        |> elem(1)
+        |> Timex.format("{ISO:Extended:Z}")
+        |> elem(1)
 
       %{
         presents: presents,
         title: title,
         description: description,
         starts_at: starts_at,
-        doors_at: starts_at,
         venue: "Come and Take It Live"
       }
     end)
   end
 
   # Covers Sidewinder and Barracuda
-  # TODO: pass in venue name
   def queueapp_json_for(url \\ "https://sidewinder.queueapp.com/feeds/events.json", venue_name \\ "Sidewinder") do
     keys = ~W[title presents description id starts_at doors_at performing]
 
@@ -120,6 +129,7 @@ defmodule VenueScraper do
     # HTTPoison.get!(url).body
     |> Poison.decode!
     |> Enum.map(fn(json) ->
+      # converts e.g. "2017-07-04 18:00 America/Chicago" into an ISO8601 string "2017-07-04T23:00:00Z"
       doors_at = Timex.parse("#{Map.get(json, "starts_at")} #{Map.get(json, "doors_at")} #{Timex.Timezone.Local.lookup}", "{YYYY}-{0M}-{0D} {h24}:{m} {Zname}")
         |> elem(1)
         |> Timex.format("{ISO:Extended:Z}")
@@ -141,20 +151,41 @@ defmodule VenueScraper do
     MapSet.size(intersection) > 0
   end
 
-  # Takes an array and indexes them into ES
+  # TODO: Use ES Bulk API instead of one-by-one
+  # Takes an array of events and indexes them into ES
   def index_events(events) do
     Enum.each(events, fn(event) ->
-      put!(@events <> "/" <> UUID.uuid4, event)
+      time = Map.get(event, :doors_at) || Map.get(event, :starts_at)
+
+      # regex removes all non-words but leaves whitespace
+      title = Regex.replace(~R/[^\w\s]/, Map.get(event, :title), "") |> String.split |> Enum.join("_")
+
+      # index end result e.g. "2017-07-21T02:00:00Z_Summer_Slam_II"
+      put!("#{@events_path}/#{time}_#{title}", event)
     end)
   end
 
-  # TODO: search across all fields
-  def full_text_search(query) do
+  def search_events_for(text) do
+    query = search([index: @events_path]) do
+      query do
+        bool do
+          should do
+            wildcard "bands", "*#{text}*"
+            wildcard "title", "*#{text}*"
+            wildcard "presents", "*#{text}*"
+          end
+        end
+      end
+    end
 
+    Tirexs.Query.create_resource(query)
   end
 
   def empty_index do
-    Tirexs.HTTP.delete(@music)
+    Tirexs.HTTP.delete(@music_path)
+  end
+
+  defp iso8601_for(date_string) do
   end
 
 end
